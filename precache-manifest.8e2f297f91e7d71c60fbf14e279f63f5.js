5e54c9db03ef10ecf985dd325e77f8509ff90101
Vicario Williams <vawilliams@github.com>
2020-07-31 17:41:52 +0200

    Updates

---
+++ precache-manifest.8e2f297f91e7d71c60fbf14e279f63f5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6f06a08fcf5b9d6a4506e3f544317b9",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "50c94d4682c72f40fc4c",
+    "url": "/Pomodoro-Clock/static/css/2.8037a259.chunk.css"
+  },
+  {
+    "revision": "eac889398e0ec3edc222",
+    "url": "/Pomodoro-Clock/static/css/main.7df09b03.chunk.css"
+  },
+  {
+    "revision": "50c94d4682c72f40fc4c",
+    "url": "/Pomodoro-Clock/static/js/2.7116e235.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro-Clock/static/js/2.7116e235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac889398e0ec3edc222",
+    "url": "/Pomodoro-Clock/static/js/main.2307cb78.chunk.js"
+  },
+  {
+    "revision": "4ccfd468b853680e7d1a",
+    "url": "/Pomodoro-Clock/static/js/runtime-main.46c75fa3.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Pomodoro-Clock/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Pomodoro-Clock/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Pomodoro-Clock/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Pomodoro-Clock/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Pomodoro-Clock/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
